50890a511bbd0656673f40fd0ade0945d1e590af
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:35:23 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
     end
   end
 end
