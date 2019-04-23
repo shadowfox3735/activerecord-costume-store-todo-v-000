043c79f723c048ca3c0d80fa742b44be119d4efc
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:31:53 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,6 +1,6 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
-def change
-  create_table :costumes do |t|
+  def change
+    create_table :costumes do |t|
       t.string :name
       t.integer :price
       t.integer :size
