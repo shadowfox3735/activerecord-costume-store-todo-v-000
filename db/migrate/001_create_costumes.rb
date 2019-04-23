1c671f3f2dfa864aaa498bf2dda67f42bce42db1
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:44:45 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.integer :size
       t.text :image_url
-      t.timestamp
+      t.timestamps
     end
   end
 end
