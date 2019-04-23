8d94593ff89d66c3f8831f4020b7d745663b58ac
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:29:14 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,8 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
-      
+      t.string :name
+      t.string :location
       
   end
 end
