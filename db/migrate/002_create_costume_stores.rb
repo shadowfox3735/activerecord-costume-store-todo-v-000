64b0454c97714a24d56b97c776a5a0982274c844
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:30:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :numberofcostumes
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      
   end
 end
