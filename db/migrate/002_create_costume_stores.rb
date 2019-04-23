bb7ace54d6679e3b793084f29f9edf48a6fc1ce9
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:31:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,6 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :costume_inventory
       t.integer :number_of_employees
       t.boolean :still_in_business
-      t.datetime :
+      t.datetime :opening_time
+      t.datetime :closing_time
   end
 end
