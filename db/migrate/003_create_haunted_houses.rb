7d7a382df5ea1b57ff2010adc21750ed630d2c8c
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:33:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,11 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   class change
-    create_table :haunted_houses
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      
+    end
   end
 end
