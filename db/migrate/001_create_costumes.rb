e759877e286ab0e46105496a4c3b699a744345c0
shadowfox3735 <normanpan180@gmail.com>
2019-04-23 17:25:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,4 @@ def change
     end
   end
 
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
-# value types according to the spec
+end
